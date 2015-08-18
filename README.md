b857c6b947bf29cc421f6f2401f02625098a1ce1
Arturo Martinez Minguez <arturo@icemobile.com>
2015-08-18 16:01:04 +0200

    Initial module setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # readcache
-Keeps a file cached in memory after reading it, until it's modified
+
+Keeps a file cached in memory after reading it, until it's modified.
---
+++ index.js
@@ -0,0 +1,47 @@
+/**
+ * @module readcache
+ */
+
+'use strict';
+
+var fs = require('fs');
+
+var cache = {};
+
+/**
+ * Reads a text file and stores it in the cache.
+ *
+ * @param {string} path - Path to the file you want to read.
+ * @param {object} [options] - Optional options object, passed to fs.readFile.
+ * @param {string} [options.encoding=utf8] - The expected string encoding, will default to `utf8`.
+ * @param {string} [options.flag] - The flag, will default to fs.readFile's
+ * @param {function} [callback] - A callback function with signature `function (err, data, stats)`
+ */
+module.exports = function (path, options, callback) {
+    if (typeof options === 'function') {
+        callback = options;
+        options = {
+            encoding: 'utf8'
+        };
+    }
+
+    options = options || {};
+    callback = callback || function () {};
+
+    fs.stat(path, function (err, stats) {
+        if (cache[path] && cache[path].since >= stats.mtime.getTime()) {
+            return callback(null, cache[path].data, { hit: true, since: cache[path].since });
+        }
+        fs.readFile(path, options, function (err, data) {
+            if (err) {
+                return callback(err);
+            }
+            var now = new Date().getTime();
+            cache[path] = {
+                data: data,
+                since: now
+            };
+            callback(null, data, { hit: false, since: now });
+        });
+    });
+};
