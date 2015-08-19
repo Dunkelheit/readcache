035484314e67e34cce3e50582087b840950a068b
Arturo Martinez Minguez <arturo@icemobile.com>
2015-08-19 10:45:50 +0200

    * Use `mtime` instead of `new Date()` when adding a new document to the cache. * Extend unit tests to reach 100% coverage

--- HISTORY.md
+++ HISTORY.md
@@ -1,3 +1,10 @@
+### 0.1.1
+* Use `mtime` instead of `new Date()` when adding a new document to the cache
+* Extend unit tests to reach 100% coverage
+
+### 0.1.0
+* Add unit test, lint and code coverage
+
 ### 0.0.1
 
 * Initial module setup
--- test/readcache.test.js
+++ test/readcache.test.js
@@ -1,14 +1,17 @@
 'use strict';
 
+var fs = require('fs');
 var path = require('path');
 var expect = require('chai').expect;
 
 var readcache = require('../lib/readcache');
 
+var testFile = path.join(__dirname, './testfile.json');
+
 describe('readcache', function () {
 
     it('Reads a file for the first time and caches it', function (done) {
-        readcache(path.join(__dirname, './testdata.json'), function (err, data, stats) {
+        readcache(testFile, function (err, data, stats) {
             expect(err).to.be.null;
             expect(data).to.be.a('string');
             expect(stats).to.be.an('object');
@@ -18,7 +21,7 @@ describe('readcache', function () {
     });
 
     it('Reads a file for the second time and returns the cached version', function (done) {
-        readcache(path.join(__dirname, './testdata.json'), function (err, data, stats) {
+        readcache(testFile, function (err, data, stats) {
             expect(err).to.be.null;
             expect(data).to.be.a('string');
             expect(stats).to.be.an('object');
@@ -27,14 +30,43 @@ describe('readcache', function () {
         });
     });
 
+    it('Refreshes the cache when the file is modified', function (done) {
+        fs.writeFile(testFile, '{"lorem": "ipsum"}', function (err) {
+            fs.stat(testFile, function (err, stats) {
+                readcache(path.join(testFile), function (err, data, stats) {
+                    expect(err).to.be.null;
+                    expect(data).to.be.a('string');
+                    expect(stats).to.be.an('object');
+                    expect(stats).to.have.property('hit', false);
+                    done();
+                });
+            });
+        });
+    });
+
     it('Allows using custom options', function (done) {
-        readcache(path.join(__dirname, './testdata.json'), { encoding: 'utf16' }, function (err, data, stats) {
+        readcache(testFile, { encoding: 'utf16' }, function (err, data, stats) {
             expect(err).to.be.null;
             expect(data).to.be.a('string');
             expect(stats).to.be.an('object');
             expect(stats).to.have.property('hit', true);
             done();
         });
+    });
+
+    it('Options can be falsy', function (done) {
+        readcache(testFile, null, function (err, data, stats) {
+            expect(err).to.be.null;
+            expect(data).to.be.a('string');
+            expect(stats).to.be.an('object');
+            expect(stats).to.have.property('hit', true);
+            done();
+        });
+    });
+
+    it('Callback may not be present', function (done) {
+        readcache(testFile);
+        done();
     });
 
     it('Throws an error when reading an unexisting file', function (done) {
